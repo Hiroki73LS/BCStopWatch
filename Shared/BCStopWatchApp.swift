05fc27d1ed928fda75bb045832e283f51a2d55f6
Hiroki mac air <aichan_lovepower@yahoo.co.jp>
2022-11-18 10:36:40 +0900

    Initial Commit

---
+++ Shared/BCStopWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BCStopWatchApp.swift
+//  Shared
+//
+//  Created by 林宏樹 on 2022/11/18.
+//
+
+import SwiftUI
+
+@main
+struct BCStopWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
